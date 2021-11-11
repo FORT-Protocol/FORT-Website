69b9eec3fb114e1f91dcb8452bde8b65bf0d70b8
tunogya <tunogya@qq.com>
2021-11-11 11:37:27 +0800

    update logo

--- src/index.tsx
+++ src/index.tsx
@@ -12,12 +12,13 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body {
-    margin: 0;
+    margin: 0px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    scroll: no;
 }
 `
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../assets/images/logo.svg';
 import styled from "styled-components";
 import bg from '../assets/images/bg.jpg';
+import fort from '../assets/images/fort.svg';
 
 const AppFrame = styled.div`
   width: 100vw;
@@ -13,9 +14,11 @@ const AppFrame = styled.div`
 `
 
 const HeaderFrame = styled.div`
+  position: absolute;
   width: 100%;
+  z-index: 9;
   display: flex;
-  padding-top: 30px;
+  padding: 30px 0;
   align-item: center;
   justify-content: space-between;
   @media (max-width: 600px) {
@@ -24,35 +27,83 @@ const HeaderFrame = styled.div`
 `
 
 const Logo = styled.img`
-margin-left: 44px;
+  margin: 0 44px;
 `
 
 const DashboardButton = styled.button`
-    height: 34px;
-    width: 170px;
-    margin-right: 44px;
-    border: 2px;
-    border-style:solid;
-    border-radius: 22px;
-    background-color: transparent;
-    color: #fff;
-    font-family: MontserratBold;
-    text-align: center;
-    font-size: 16px;
-    line-height: 20px;
-    cursor:pointer;
+  height: 34px;
+  width: 170px;
+  margin-right: 44px;
+  border: 2px;
+  border-style:solid;
+  border-radius: 22px;
+  background-color: transparent;
+  color: #fff;
+  font-family: MontserratBold;
+  text-align: center;
+  font-size: 16px;
+  line-height: 20px;
+  cursor:pointer;
+`
+
+const ContentFrame = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const BallsFrame = styled.div`
+  height: 100%;
+  width: 100%;
+  
+  position: absolute;
+  z-index: 5;
+`
+
+const AppButton = styled.button`
+  height: 34px;
+  width: 146px;
+  margin-right: 44px;
+  border: 2px;
+  border-style:solid;
+  border-radius: 22px;
+  background-color: transparent;
+  color: #fff;
+  font-family: MontserratBold;
+  text-align: center;
+  font-size: 16px;
+  line-height: 20px;
+  cursor:pointer;
+`
+
+const FormulaImage = styled.img`
+  width: 530px;
+  height: 70px;
+  margin-bottom: 30px;
 `
 
 function App() {
   return (
     <AppFrame>
+      <BallsFrame>
+      </BallsFrame>
       <HeaderFrame>
         <Logo src={logo} alt="logo"/>
         <DashboardButton>
           Dashboard
         </DashboardButton>
       </HeaderFrame>
-
+      <ContentFrame>
+        <FormulaImage src={fort} alt="fort formula"/>
+        <AppButton>
+          APP
+        </AppButton>
+      </ContentFrame>
     </AppFrame>
   );
 }
